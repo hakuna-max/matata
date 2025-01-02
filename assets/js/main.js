7acf9adbddc0ede685dbb85aac9e4bd315c928b0
Peachy@upc.edu.cn <rolf.zhang@outlook.com>
2025-01-02 12:46:20 +0800

    Customize 404 page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,9 @@
 // import setupMainScale from "js/utils/scale-main";
+import { setupDocumentReady, initializeSvgAnimations } from 'js/animation/page-lost';
+
+// 使用 setupDocumentReady 初始化文档加载时的动画
+setupDocumentReady();
+initializeSvgAnimations();
 
 
 // setupMainScale();
